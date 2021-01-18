88f10b7652fcac2cdb3d053595abfa47aaf09dbc
Syafrudin - <rizkyfauzi21303@gmail.com>
2021-01-18 16:10:51 +0700

    aritmatika

---
+++ aritmatika.py
@@ -0,0 +1,25 @@
+x = 8
+y = 3
+z = 2
+print ("Nilai A:",x)
+print ("Nilai B:",y)
+print ("Nilai C:",z)
+print ("=====Hasil Penjumlahan=====")
+Hasil = x + y
+print ("A + B = ",Hasil)
+Hasil = x - y
+print ("A - B = ",Hasil)
+Hasil = x * y
+print ("A * B = ",Hasil)
+Hasil = x / y
+print ("A / B = ",Hasil)
+Hasil = x % y
+print ("A % B = ",Hasil)
+Hasil = x // y
+print ("A // B = ",Hasil)
+Hasil = x ** y
+print ("A ** B = ",Hasil)
+Hasil = (x - y) / z
+print ("(A - B) / C = ",Hasil)
+Hasil = x - y / z
+print ("A - B / C = ",Hasil)
